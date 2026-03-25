da1d63c263372784937cf33aa05ccd55613b7ea9
MD. Raisul Islam Niloy <raisulislamniloy47@gmail.com>
2026-03-25 22:33:10 +0600

    first commit

---
+++ src/main/java/com/example/warehouse/module_16_assignment/OstadJavaSpringBoot3Module16AssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.warehouse.module_16_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OstadJavaSpringBoot3Module16AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OstadJavaSpringBoot3Module16AssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/warehouse/module_16_assignment/OstadJavaSpringBoot3Module16AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warehouse.module_16_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OstadJavaSpringBoot3Module16AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
